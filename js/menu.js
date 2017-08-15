84a9ed30fd5db038a64d150eda5aa1bbcb82077e
2000jester <2000jester@gmail.com>
2017-08-16 09:40:52 +1000

    making menu use the global username variable

---
+++ js/menu.js
@@ -0,0 +1,1 @@
+document.getElementById("menu-name-container").value = document.getElementById("menu-name-container").value + username
--- js/title.js
+++ js/title.js
@@ -1,4 +1,12 @@
-window.addEventListener('keydown', onKeyDown, false);
-function onKeyDown(evt){
-    
+document.getElementById("title-name-input").value = "";
+document.getElementById("title-name-prompt").style.fontSize = "17px";
+document.getElementById("title-name-prompt").style.color = "white";
+function proceedCheck(){
+    if(document.getElementById("title-name-input").value != ""){
+        username = document.getElementById("title-name-input").value;
+        window.location.href='html/menu.html';
+    } else {
+        document.getElementById("title-name-prompt").style.fontSize = "20px";
+        document.getElementById("title-name-prompt").style.color = "#ff1a1a";
+    }
 }
