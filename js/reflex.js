7397423b6ecbbf9b65854bc5545e6a4a75757385
OrmusELdesu <ormusldesu@gmail.com>
2017-08-16 18:39:10 +1000

    changed a comment

--- js/reflex.js
+++ js/reflex.js
@@ -1,4 +1,4 @@
-// contains alphabetically arranged keycode for letters
+// contains alphabetically arranged keycode for letters and numbers
 var char_keys = [48,49,50,51,52,53,54,55,56,57,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90]
 // choose between a letter or a number key
 var assigned_key = char_keys[random(0, char_keys.length-1)], // stores the randomly generated key from the array above
