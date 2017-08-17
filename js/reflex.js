f0dee3262d683cc442daa8ba16558c816d36e96c
2000jester <2000jester@gmail.com>
2017-08-17 21:34:26 +1000

    fixed switch in reflex.js

--- js/reflex.js
+++ js/reflex.js
@@ -69,33 +69,17 @@ function startTimer(duration, display)
 
         var tempSecondsSplit = seconds.split("");//splits seconds so that a one digit value can be displayed
         display.textContent = tempSecondsSplit[1];//sets the elements text to equal the temp value
-    
-        // the switch statement is always watching you 0_0
-        /*
-        switch(tempSecondsSplit[1])
-        {
-            case (tempSecondsSplit[1] == 3):
-            {
+        switch(tempSecondsSplit[1]){//switch statement that selects the text color based on what number it is
+            case "3":
                 document.getElementById("reflex-single").style.color = "red";
-            }
-            case (tempSecondsSplit[1] == 2):
-            {
+                break;
+            case "2":
                 document.getElementById("reflex-single").style.color = "orange";
-            }
-            case (tempSecondsSplit[1] == 1):
-            {
+                break;
+            case "1":
                 document.getElementById("reflex-single").style.color = "limegreen";
-            }
-        }*/
-        
-        if(tempSecondsSplit[1] == "3"){
-            document.getElementById("reflex-single").style.color = "red";
-        } else if(tempSecondsSplit[1] == "2"){
-            document.getElementById("reflex-single").style.color = "orange";
-        } else if(tempSecondsSplit[1] == "1"){
-            document.getElementById("reflex-single").style.color = "limegreen";
+                break;
         }
-        
         document.getElementById("reflex-single").style.marginRight = "88%";//re aligns the text
         
         time_limit--;
