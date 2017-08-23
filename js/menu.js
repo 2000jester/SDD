d91dcf9f3b29957a58ed4af8994865e66b2eab3a
2000jester <2000jester@gmail.com>
2017-08-23 23:46:25 +1000

    added from generic script

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,3 @@
-window.onbeforeunload = function(){
-    prompt("Do you want to exit")
-}
-function exit(){
-    //window.location.href="https://www.google.com"
+function close(){
+    window.close();
 }
